c8aa2e8b987814a7ff52ec418ce0cfe093d6e1f0
jesus <jesuscalles2001@gmail.com>
2025-02-27 09:20:11 -0400

    mejoras

--- config/db.js
+++ config/db.js
@@ -11,4 +11,14 @@ const pool = mysql.createPool({
 
 const db = pool.promise();
 
+// Test connection to the database
+db.getConnection()
+  .then(connection => {
+    console.log('Successfully connected to the database');
+    connection.release(); // Release the connection back to the pool
+  })
+  .catch(error => {
+    console.error('Error connecting to the database:', error.message);
+  });
+
 module.exports = db;
--- server.js
+++ server.js
@@ -4,18 +4,17 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const morgan = require('morgan');
 
 // Middlewares
 app.use(bodyParser.json());
-// Enable CORS for all routes
-app.use(cors());
-
-// Or enable CORS for specific origins
-app.use(
-  cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
-  })
-);
+// Enable CORS for all origins (allow all connections)
+app.use(cors({
+  origin: '*',  // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Specify allowed headers
+}));
+app.use(morgan('dev'));
 
 // Rutas
 app.use('/api/sociedad', require('./routes/sociedad'));
@@ -24,15 +23,6 @@ app.use('/api/directivalocal', require('./routes/directivaLocal'));
 app.use('/api/directivazonal', require('./routes/directivaZonal'));
 app.use('/api/login', require('./routes/login'));
 app.use('/api/iglesia', require('./routes/igelsias'));
-
-
-
-//
-//app.use('/api/zona', require('./routes/zona'));
-//app.use('/api/iglesia', require('./routes/iglesia'));
-//app.use('/api/cargo', require('./routes/cargo'));
-//app.use('/api/directiva-local', require('./routes/directivaLocal'));
-//app.use('/api/directiva-zonal', require('./routes/directivaZonal'));
 
 // Iniciar servidor
 app.listen(PORT, () => {
