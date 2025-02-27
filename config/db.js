a826da15c38a7731b08e6d6c7f430c2f96da58e4
jesus <jesuscalles2001 @gmail.com>
2025-02-26 23:27:22 -0400

    cambios2

--- config/db.js
+++ config/db.js
@@ -1,18 +1,14 @@
-require('dotenv').config();
 const mysql = require('mysql2');
+require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
 });
 
-connection.connect(err => {
-  if (err) {
-    console.error('Error de conexión:', err);
-    return;
-  }
-  console.log('Conectado a la base de datos');
-});
+const db = pool.promise();
+
+module.exports = db;
